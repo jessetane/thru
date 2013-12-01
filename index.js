5d90d74e244f0096807e8e08de0845c434d702fa
Jesse Tane <jesse.tane@gmail.com>
2013-12-01 02:42:29 -0500

    make inheritable

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var stream = require('stream');
+var Transform = require('_stream_transform');
+var inherits = require('inherits');
 
-module.exports = function(transformer) {
-  var t = new stream.Transform({ objectMode: true });
-  t._transform = function(chunk, encoding, cb) {
-    if (transformer) {
-      transformer.call(this, chunk, function(err, data) {
-        if (data) {
-          t.push(data);
-        }
-        cb(err);
-      });
-    }
-    else {
-      t.push(chunk);
-      cb();
-    }
-  };
-  return t;
+module.exports = Thru;
+
+function Thru(transformer, opts) {
+  if (!(this instanceof Thru))
+    return new Thru(transformer, opts);
+
+  this.transformer = transformer;
+
+  opts = opts || {};
+  opts.objectMode = true;
+  
+  Transform.call(this, opts);
+}
+inherits(Thru, Transform);
+
+Thru.prototype._transform = function(chunk, encoding, cb) {
+  if (this.transformer) {
+    var self = this;
+    this.transformer(chunk, function(err, data) {
+      if (data) {
+        self.push(data);
+      }
+      cb(err);
+    });
+  }
+  else {
+    this.push(chunk);
+    cb();
+  }
 };
